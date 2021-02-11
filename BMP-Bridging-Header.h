dd3e85ee937c788e4676fa5904bd82eee838aecf
Tim Van Cauwenberge <cloutking619@gmail.com>
2021-02-10 22:18:06 -0800

    First Commit

---
+++ BMP-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  BMP-Bridging-Header.h
+//  BMP
+//
+//  Created by Timmy Van Cauwenberge on 2/4/21.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
---
+++ BMP/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  BMP
+//
+//  Created by Timmy Van Cauwenberge on 2/4/21.
+//
+
+import Foundation
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"BMP://")!
+let spotifyClientId = "5cdef1396822465fb3867f892bfe6b25"
+let spotifyClientSecretKey = "e26fa13214564d909f1bf2b9c471bc9b"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
